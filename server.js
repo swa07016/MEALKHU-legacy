f8ee90f5dbc77bbce41dcd5d05b0bffec93fec98
swa07016 <swa07016@naver.com>
2020-06-07 18:54:20 +0900

    mysql DB연결(aws rds)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const mysql = require('mysql');
 var iconv  = require('iconv-lite');
 
 const app = express();
@@ -8,9 +9,23 @@ const port = process.env.PORT || 5000;
 
 const dataBuffer = fs.readFileSync('json_datas.json')
 
+
+// db connection
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.get('/api/hello', (req, res) => {
     res.send('Hello skrrrr!');
